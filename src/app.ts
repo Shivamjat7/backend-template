49dc4dc5531a97995f55fda8504a4c791c0773e2
Shivamjat7 <shivamjat531@gmail.com>
2026-06-03 11:53:59 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app =  express();
+
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({msg:'hello from backend'})
+})
+
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from './app';
+
+const PORT = process.env.PORT || 3000
+
+
+const server = app.listen(PORT,()=>{
+
+
+    console.log(`server is running on http://localhost:${PORT}`)
+})
+
+export default server;
